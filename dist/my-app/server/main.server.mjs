539349b1040fc3e181801c55ecf6a0d1eefde073
tktktakatuska <blue.mountain.syota@icloud.com>
2024-01-16 22:08:05 +0900

    hoge

--- dist/my-app/server/main.server.mjs
+++ dist/my-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VQGLH62E.mjs";import"./chunk-VIWDEWN4.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-KVON5T5B.mjs";import"./chunk-VIWDEWN4.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Routes } from '@angular/router';
 import { HogeHogeComponent } from './hoge-hoge/hoge-hoge.component';
 import { MenuComponent } from './menu/menu.component';
-import { PathNotFoundComponentComponent } from './path-not-found-component/path-not-found-component.component';
 
 
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'hoge-hoge', component: HogeHogeComponent },
-    { path: 'menu', component: MenuComponent },
-    { path: '**', component:  PathNotFoundComponentComponent}  // Wildcard route for a 404 page
+    { path: 'menu', component: MenuComponent }
+    
 ];

658d801b8875e0b03252de9362e925167e7e7b52
tktktakatuska <blue.mountain.syota@icloud.com>
2024-01-19 08:08:03 +0900

    /menu api実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
 import { MenuComponent } from "./menu/menu.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HogeHogeComponent } from "./hoge-hoge/hoge-hoge.component";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, MenuComponent, DashboardComponent, HogeHogeComponent]
+    imports: [CommonModule, RouterOutlet, MenuComponent, DashboardComponent, HogeHogeComponent, MatSlideToggleModule, MatIconModule, MatToolbarModule, MatButtonModule, FlexLayoutModule]
+    
 })
 export class AppComponent {
   title = 'MyApp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Routes } from '@angular/router';
 import { HogeHogeComponent } from './hoge-hoge/hoge-hoge.component';
 import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'hoge-hoge', component: HogeHogeComponent },
-    { path: 'menu', component: MenuComponent }
+    { path: 'menu', component: MenuComponent },
+    { path: '404', component: NotFoundComponent },
+    
+    
+    // すべての他のパスは404ページにリダイレクト
+    { path: '**', redirectTo: '/404' }
     
 ];
 

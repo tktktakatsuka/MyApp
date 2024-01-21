1f6f6ddb97683d7619a55db8fbe82c17e236b92a
tktktakatuska <blue.mountain.syota@icloud.com>
2024-01-22 01:03:27 +0900

    Auto-generated commit [ci skip]

---
+++ server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-DD63RBX5.mjs";
+import "./chunk-2C3F4U4U.mjs";
+import "./chunk-MD6NWHS6.mjs";
+export {
+  main_server_default as default
+};
+//# sourceMappingURL=main.server.mjs.map

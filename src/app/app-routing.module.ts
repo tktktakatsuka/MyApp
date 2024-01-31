ed72bddfdf95a980cbcd201006788ee935bb36b5
tktktakatuska <blue.mountain.syota@icloud.com>
2024-01-31 21:01:58 +0900

    menuバー追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 
 const routes: Routes = [];
     
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

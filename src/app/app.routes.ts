7f9477cc87b6858a58b5da048f15bba5461a8c94
tktktakatuska <blue.mountain.syota@icloud.com>
2024-02-01 21:21:59 +0900

    hoge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Routes } from '@angular/router';
 import { MenuComponent } from './shohin/menu.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 
 
 
 export const routes: Routes = [
-    { path: '', component: MenuComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'shohin', component: MenuComponent },
+    { path: '', component: MenuComponent }
+    ,{ path: 'dashboard', component: DashboardComponent }
+    ,{ path: 'shohin', component: MenuComponent }
+    ,{ path: 'recipe', component: RecipeComponent }
     
 ];
 

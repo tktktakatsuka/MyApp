130e521d863e9c6d7cb34e4b813b666b2f26b9ef
tktktakatuska <blue.mountain.syota@icloud.com>
2024-02-08 00:23:03 +0900

    003追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MenuComponent } from './shohin/menu.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { article001Component } from './article001/article001.component';
 import { article002Component } from './article002/article002.component';
+import { article003Component } from './article003/article003.component';
 
 
 
@@ -15,6 +16,7 @@ export const routes: Routes = [
     ,{ path: 'recipe', component: RecipeComponent }
     ,{ path: 'article001', component: article001Component }
     ,{ path: 'article002', component: article002Component }
+    ,{ path: 'article003', component: article003Component }
 
     
 ];
